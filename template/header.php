dfc12460a8ca30f1351f470877cdaa9295c54de6
Wayne Merricks <waynemerricks@thevoiceasia.com>
2016-03-19 22:06:54 +0000

    Added jquery to header

--- template/header.php
+++ template/header.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title><?php echo $title; ?></title>
-        <script src="js/<?php echo $js; ?>" type="text/javascript"></script>
-        <link href="css/<?php echo $css; ?>" rel="stylesheet" type="text/css">
-    </head>
-    <body>
+  <head>
+    <title><?php echo $title; ?></title>
+    <script src="js/<?php echo $js; ?>" type="text/javascript"></script>
+    <script src="../../libs/jquery-2.2.2.min.js" type="text/javascript"></script>
+    <link href="css/<?php echo $css; ?>" rel="stylesheet" type="text/css">
+  </head>
+  <body>
