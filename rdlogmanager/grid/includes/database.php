f8e142499942fe308c5b04e46bf4780f146b5987
Wayne Merricks <waynemerricks@thevoiceasia.com>
2016-03-19 00:46:25 +0000

    Implemented getGrid, we don't need getServices.

--- rdlogmanager/grid/includes/database.php
+++ rdlogmanager/grid/includes/database.php
@@ -27,7 +27,11 @@
 
   }
 
-  //TODO
+  /**
+   * Gets the service names in the RIV DB
+   * @param $PDO: PDO Connection to use
+   * @return array of service names
+   */
   function getServiceNames($PDO){
 
     $services = array();
@@ -50,10 +54,36 @@
 
   }
 
-  function getServices($PDO){}
+  /**
+   * Gets the full grid for the specified service
+   * @param $PDO: PDO Connection to use
+   * @param $service: Service Name to get
+   * @return array of service including all 168 clocks
+   */
+  function getGrid($PDO, $service){
 
-  function getGrid($PDO, $service){}
+    $grid = NULL;
 
+    $sql = 'SELECT * FROM `SERVICES` WHERE `NAME` = :serviceName';
+
+    $stmt = $PDO->prepare($sql);
+    $stmt->bindParam(':serviceName', $service);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    while($row = $stmt->fetch()){
+
+      $grid = $row;
+
+    }
+
+    $stmt = NULL;
+
+    return $grid;
+
+  }
+
+  //TODO
   function updateServiceGrid($PDO, $service, $grid){}
 
 ?>
