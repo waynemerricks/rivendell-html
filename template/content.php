971755777f4f81edfbf1e7fb189f16f18b2b8b59
Wayne Merricks <waynemerricks@thevoiceasia.com>
2016-03-20 23:28:31 +0000

    Basic template file for content

---
+++ template/content.php
@@ -0,0 +1,18 @@
+<?php
+
+  //Set Page Title, link to js and css
+  $title = 'Rivendell Grid';
+  $js = 'grid.js';
+  $css = 'grid.css';
+
+  include('header.php');//Header HTML
+
+?>
+
+<!-- Content Goes Here -->
+
+<?php //Footer
+
+  include('footer.php');//Footer HTML
+
+?>
