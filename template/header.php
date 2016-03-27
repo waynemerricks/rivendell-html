6d61cc8e42fcb55ca5b8b8c3a75f7ea3382657c1
Wayne Merricks <waynemerricks@thevoiceasia.com>
2016-03-27 21:39:45 +0100

    Added multiple JS support to header javascript

--- template/header.php
+++ template/header.php
@@ -2,7 +2,14 @@
 <html lang="en">
   <head>
     <title><?php echo $title; ?></title>
-    <script src="js/<?php echo $js; ?>" type="text/javascript"></script>
+<?php
+  //Insert JS Scripts
+
+  foreach($js as $script){
+
+?>
+    <script src="js/<?php echo $script; ?>" type="text/javascript"></script>
+<?php } ?>
     <script src="../../libs/jquery-2.2.2.min.js" type="text/javascript"></script>
     <link href="css/<?php echo $css; ?>" rel="stylesheet" type="text/css">
   </head>
